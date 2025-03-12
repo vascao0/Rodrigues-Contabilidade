d59b1ab074f88697aeed5c87c3c28c7cd513d968
Pedro Freitas <pedrofreitassantos2021@gmail.com>
2025-03-11 22:01:53 -0300

    talvez concluido

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ menu.addEventListener('click', ()=> {
 overlay.addEventListener('click', ()=> {
     menu.classList.remove('abrir-menu');
 });
+
+  document.getElementById("meuFormulario").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let nome = document.getElementById("name").value;
+    let servico = document.getElementById("servico").value;
+    let regime = document.getElementById("regime").value;
+    let area = document.getElementById("area").value;
+    let faturamento = document.getElementById("faturamento").value;
+
+    let mensagem = `Olá, eu sou "${nome}", preciso "${servico}", a área de atuação da minha empresa é "${area}", meu regime tributário atual é "${regime}" e a minha média de faturamento é "${faturamento}".`;
+
+    let numeroWhatsApp = "77988449348";
+    let link = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem)}`;
+
+    window.open(link, "_blank");
+  });
